7cf5ff537381a11ac0a86d1007709fafe334b8ce
iesoveev <iesoveev@gmail.com>
2024-10-10 15:24:23 +0400

    Fixed register

--- src/main/java/org/example/demodanya/try2/config/SecurityConfig.java
+++ src/main/java/org/example/demodanya/try2/config/SecurityConfig.java
@@ -1,41 +1,38 @@
 package org.example.demodanya.try2.config;
 
+import lombok.RequiredArgsConstructor;
 import org.example.demodanya.try2.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Lazy
-    @Autowired
-    private CustomUserDetailsService userDetailsService;
+    private final CustomUserDetailsService userDetailsService;
 
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authz -> authz
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/auth/register", "/auth/login").permitAll()
                         .requestMatchers("/user/resource").hasRole("USER")
                         .requestMatchers("/admin/resource").hasRole("ADMIN")
                         .anyRequest().authenticated()
-                )
-                .formLogin(form -> form.permitAll())
-                .logout(logout -> logout.permitAll());
+                );
 
         return http.build();
     }
